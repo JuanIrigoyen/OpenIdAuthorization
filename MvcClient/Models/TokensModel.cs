824fe0285ba1aed23b01ee241f8237643e630716
Juan Irigoyen <juan_irigoyen@hotmail.com>
2020-02-19 22:51:33 +0100

    Add files via upload

---
+++ MvcClient/Models/TokensModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcClient.Models
+{
+    public class TokensModel
+    {
+        public string IdToken { get; set; }  
+        
+        public string AccessToken { get; set; }
+    }
+}
