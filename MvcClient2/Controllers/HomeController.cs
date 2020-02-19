c1d9311b44849d8608507ee680a117401bd43e92
Juan Irigoyen <juan_irigoyen@hotmail.com>
2020-02-19 22:54:12 +0100

    Add files via upload

---
+++ MvcClient2/Controllers/HomeController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcClient.Models;using IdentityModel;
+using IdentityModel.Client;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
+
+namespace MvcClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Privacy()
+        {
+            var access_token = await HttpContext.GetTokenAsync("access_token");
+            var id_token = await HttpContext.GetTokenAsync("id_token");
+            return View(new TokensModel() { AccessToken = access_token, IdToken = id_token });
+        }
+
+        [Authorize]
+        public IActionResult Logout()
+        {
+            return new SignOutResult()
+            {
+                AuthenticationSchemes = new List<string>()
+                {
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    OpenIdConnectDefaults.AuthenticationScheme
+                }
+            };
+        }
+
+        [HttpGet]
+        public IActionResult FrontChannelLogout(string id, string iss)
+        {
+            return new SignOutResult()
+            {
+                AuthenticationSchemes = new List<string>()
+                {
+                    CookieAuthenticationDefaults.AuthenticationScheme
+                }
+            };
+        }
+
+        [HttpPost]
+        public IActionResult BackChannelLogout(string logout_token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var data = tokenHandler.ReadJwtToken(logout_token);
+          
+             //var isValid = 
+            var subject = data.Claims.Where(c => c.Type == JwtClaimTypes.Subject)
+                              .SingleOrDefault();
+            return Ok();
+
+
+            return new SignOutResult()
+            {
+                AuthenticationSchemes = new List<string>()
+                {
+                    CookieAuthenticationDefaults.AuthenticationScheme
+                }
+            };
+        }
+
+        private async Task<bool> IsValid(string logout_token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var httpClient = new HttpClient();
+            var document = await httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest()
+            {
+                Address = "https://locahost:4438"
+            });
+
+            var keys = new List<SecurityKey>();
+
+            foreach (var item in document.KeySet.Keys)
+            {
+                var exponent = Base64Url.Decode(item.E);
+                var modulo = Base64Url.Decode(item.N);
+
+                var key = new RsaSecurityKey(new RSAParameters()
+                {
+                    Modulus = modulo,
+                    Exponent = exponent
+                });
+
+                keys.Add(key);
+            }
+
+            var tokenValidtationParameters = new TokenValidationParameters()
+            {
+                ValidIssuer = document.Issuer,
+                ValidAudience = "hybrid-sample",
+                IssuerSigningKeys = keys
+            };
+
+            var principal = tokenHandler.ValidateToken(logout_token, tokenValidtationParameters, out _);
+            return principal != null;
+        }
+
+
+        [Authorize]
+        public async Task<IActionResult> WheatherForecast()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var access_token = await HttpContext.GetTokenAsync("access_token");
+                httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {access_token}");
+
+                var response = await httpClient.GetAsync("https://localhost:44331/WeatherForecast");
+                if (response.IsSuccessStatusCode)
+                { 
+                    return Redirect("Privacy");
+                }
+                else return Redirect("Index");
+            }
+        }
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MvcClient2/Startup.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+
+namespace MvcClient
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddAuthentication(setup =>
+            {
+                setup.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                setup.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddCookie(setup =>
+            {
+                setup.Cookie.Name = "AppCookie";
+                setup.ExpireTimeSpan = TimeSpan.FromSeconds(10);
+                //setup.Cookie.Expiration = TimeSpan.FromSeconds(10);
+            })
+            .AddOpenIdConnect(setup =>
+            {
+                // Setup own identity server
+                setup.Authority = "https://localhost:44303";
+                setup.ClientId = "hybrid-sample2";
+                setup.ClientSecret = "secret";
+                setup.ResponseType = "code id_token";
+
+                setup.Scope.Clear();
+                setup.Scope.Add("openid");
+                setup.Scope.Add("profile");
+                setup.Scope.Add("email");
+                setup.Scope.Add("sampleapi");
+                setup.Scope.Add("workshopprofile");
+
+                setup.SaveTokens = true;
+
+                //// let you to get the user information in diferent channels
+                //// setup.GetClaimsFromUserInfoEndpoint = true;
+
+                //// setup.ClaimActions.Remove("WorkshopClaimType");
+                //// setup.ClaimActions.MapUniqueJsonKey("pepito", "WorkshopClaimType");
+
+                //setup.CallbackPath = "/callback";
+                setup.SignedOutCallbackPath = "/signout-callback-oidc";
+                setup.SignedOutRedirectUri = "/home/index";
+
+                setup.ClaimActions.MapUniqueJsonKey("pepito", "WorkshopClaimType");
+
+                //// Setup names claims and roles for found when Microsoft is not the provider
+                //setup.TokenValidationParameters = new TokenValidationParameters()
+                //{
+                //    ClockSkew = new TimeSpan(10000),
+                //    NameClaimType = "claim",
+                //    RoleClaimType = "role",
+                //    RequireSignedTokens = false
+                //};
+
+                // To force the client sign if you close the browser
+                //setup.Events.OnRedirectToIdentityProvider = context =>
+                //{
+                //    context.ProtocolMessage.Prompt = Configuration["Security:Prompt"];
+                //    return Task.CompletedTask;
+                //};
+
+                //// Setup names claims and roles for found when Microsoft is not the provider
+                //setup.TokenValidationParameters = new TokenValidationParameters()
+                //{
+                //    ClockSkew = new TimeSpan(10000)
+                //};
+
+                // Use when you need that timelife cookie is the same that the Token. It´s a good practice
+                setup.UseTokenLifetime = true;
+
+                //// Let you to open page with last state
+                //setup.Events.OnTicketReceived = context =>
+                //{
+                //    context.Properties.IsPersistent = true;
+                //    return Task.CompletedTask;
+                //};
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
